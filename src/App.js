2a2c6e4c114fcf514f4b85535a1c7f593236be78
imranyy <imranmatano6@gmail.com>
2022-03-14 18:08:28 -0700

    using useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import Form from './components/form';
+import { useState } from 'react';
 
 function App() {
+  const [tasks,useTasks]= useState(
+    [
+      {
+        id:1,
+        tast1:'firebase Auth',
+        task2:'machine learning'
+      },
+      {
+        id:2,
+        tast1:'firebase Auth',
+        task2:'machine learning'
+      }
+    ]
+  )
   return (
     <div className="App">
-      <Navbar title={Todo}/>
+      <Navbar title="Todo"/>
       <Form/>   
+      {tasks.map((task)=>(
+        <h3 key={task.id}>{task.tast1}</h3>
+      ))}
     </div>
   );
 }
